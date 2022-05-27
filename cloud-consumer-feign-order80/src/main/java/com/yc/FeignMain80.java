f0ba71081f2bda62bcbc74b92d6d74827f76a8e7
yang_yang <sdzoyy@163.com>
2022-05-27 19:51:45 +0800

    master分支提交

--- cloud-consumer-feign-order80/src/main/java/com/yc/FeignMain80.java
+++ cloud-consumer-feign-order80/src/main/java/com/yc/FeignMain80.java
@@ -1,6 +1,5 @@
 package com.yc;
 
-import feign.Feign;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
